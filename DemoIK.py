ff41d2bd7f070ee04d658328ee446c3e8b60b2c4
root <rorschachuk@gmail.com>
2014-06-14 16:23:21 +0000

    Baseline from Pi version

---
+++ DemoIK.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  DemoIK.py - York Hack Space May 2014
+#  Simple demo of meArm library to walk through some points defined in Cartesian coordinates
+
+import meArm
+
+def main():
+    arm = meArm.meArm()
+    arm.begin()
+	
+    while True:
+        arm.openGripper()
+        arm.closeGripper()
+        arm.openGripper()
+        arm.closeGripper()
+        arm.openGripper()
+
+        arm.gotoPoint(0, 150, 50)
+        arm.gotoPoint(0, 150, 0)
+        arm.gotoPoint(0, 150, 150)
+        arm.gotoPoint(0, 150, 50)
+        arm.gotoPoint(-150, 150, 50)
+        arm.gotoPoint(150, 150, 50)
+        arm.gotoPoint(0, 150, 50)
+        arm.gotoPoint(0, 100, 50)
+	return 0
+
+if __name__ == '__main__':
+    main()
